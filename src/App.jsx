d604266ca5d9e0e19b13cd19c216e9462ac8535c
Anant-001 <anantdevliyal01@gmail.com>
2026-04-23 16:59:27 +0530

    Cocktail Menu

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import gsap from "gsap";
 import { ScrollTrigger, SplitText } from "gsap/all";
 import Navbar from "./Components/Navbar.jsx";
 import Hero from "./Components/Hero.jsx";
+import Cocktails from "./Components/Cocktails.jsx";
+
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
@@ -11,6 +13,7 @@ const App = () => {
         <main>
             <Navbar />
             <Hero />
+            <Cocktails />
         </main>
     )
 }
